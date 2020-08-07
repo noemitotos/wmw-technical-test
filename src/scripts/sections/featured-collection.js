3039caa25a996b84b019f2f67f7c6cdd521ee090
Noemi Totos <noemi@inspiradigital.co.uk>
2020-08-07 09:59:45 +0100

    Featured collection development

---
+++ src/scripts/sections/featured-collection.js
@@ -0,0 +1,25 @@
+/**
+ * Section: Featured collection
+ * ------------------------------------------------------------------------------
+ * Featured collection configuration for complete theme support
+ * - https://github.com/Shopify/theme-scripts/tree/master/packages/theme-sections
+ *
+ * @namespace featuredCollection
+ */
+import {register} from '@shopify/theme-sections';
+
+/**
+ * Featured collection constructor
+ * Executes on page load as well as Theme Editor `section:load` events.
+ *
+ * @param {string} container - selector for the section container DOM element
+ */
+register('featured-collection', {
+  init() {
+    this.publicMethod();
+  },
+
+  publicMethod() {
+    window.console.log('Initialising featured collection section');
+  },
+});
